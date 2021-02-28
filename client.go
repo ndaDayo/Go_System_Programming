b0c4de2604fb7b75be337e9e00aa7d17a44ee1ea
hayato sugawara <hayato711@gmail.com>
2021-02-28 18:21:48 +0900

    format

--- client.go
+++ client.go
@@ -9,24 +9,24 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp","localhost:8888")
-	if err != nil{
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
 		panic(err)
 	}
 	request, err := http.NewRequest(
-		"GET","http://localhost:8888", nil)
-    if err != nil{
+		"GET", "http://localhost:8888", nil)
+	if err != nil {
 		panic(err)
 	}
 	request.Write(conn)
 	response, err := http.ReadResponse(
 		bufio.NewReader(conn), request)
-	if err != nil{
+	if err != nil {
 		panic(err)
-	}	
-	dump, err := httputil.DumpResponse(response,true)
+	}
+	dump, err := httputil.DumpResponse(response, true)
 
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(dump))
--- server.go
+++ server.go
@@ -10,40 +10,40 @@ import (
 	"strings"
 )
 
-func main(){
-	listener, err := net.Listen("tcp","localhost:8888")
-	if(err != nil){
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Server is running at localhost:8888")
 
-	for{
+	for {
 		conn, err := listener.Accept()
-	    if(err != nil){
-           panic(err)
-      	}
-        go func ()  {
+		if err != nil {
+			panic(err)
+		}
+		go func() {
 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
 
-		request, err := http.ReadRequest(
-			bufio.NewReader(conn))
-		if (err != nil){
-			panic(err)
-		}
-		dump, err := httputil.DumpRequest(request,true)
-		if(err != nil){
-			panic(err)
-		}
-		fmt.Println(string(dump))
-		response := http.Response{
-			StatusCode: 200,
-			ProtoMajor: 1,
-			ProtoMinor: 0,
-			Body: ioutil.NopCloser(strings.NewReader("Hello World\n")),
-		}
-		response.Write(conn)
-		conn.Close()
+			request, err := http.ReadRequest(
+				bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
 		}()
-    }
+	}
 }
